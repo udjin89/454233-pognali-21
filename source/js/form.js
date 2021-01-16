25886b724c975bdae30c4b5584f0ecbddee86fb3
Евгений <jenek95@mail.ru>
2021-01-16 15:04:20 +0300

    catalog2

--- source/js/form.js
+++ source/js/form.js
@@ -1,6 +1,21 @@
 
+const addArrow = document.querySelectorAll(".button-next-step");
+// +++++++++++++ Проверка размера экрана
+let sizeWindow = window.innerWidth;
+console.log("Size Window =" + sizeWindow);
+if (sizeWindow > 768) {
+  // console.log("Size Window =" + sizeWindow);
+  for (let i = 0; i < addArrow.length; i++) {
+    addArrow[i].classList.add("button-arrow");
+  }
+}
+else {
+  for (let i = 0; i < addArrow.length; i++) {
+    addArrow[i].classList.remove("button-arrow");
+  }
+}
+// +++++++++++ добавление стрелочки при изменинии экрана
 window.addEventListener(`resize`, event => {
-  const addArrow = document.querySelectorAll(".button-next-step");
   if (screen.width > 768) {
     console.log(screen.width);
     for (let i = 0; i < addArrow.length; i++) {
@@ -13,3 +28,41 @@ window.addEventListener(`resize`, event => {
     }
   }
 }, false);
+//++++++ Выпадающая таблица со странами
+const chose = document.querySelectorAll(".add-plan__item--select");
+console.log("chose = " + chose[0]);
+if (chose) {
+
+  for (let i = 0; i < chose.length; i++) {
+    // let removeButton = 0;
+    let buttonCloseChose = chose[i].querySelector(".remove-country");
+
+    // buttonCloseChose.addEventListener("click", function (evt) {
+    //   evt.preventDefault();
+    //   console.log("Click Remove Button \n");
+    //   removeButton = 1;
+    //   console.log("-> " + removeButton);
+    //   if (1) {
+
+    //   }
+    //   else {
+
+    //   }
+    // });
+
+    chose[i].addEventListener("click", function (evt) {
+      evt.preventDefault();
+      console.log("click chose country");
+      if (!chose[i].classList.contains("add-plan__item--select-active")) {
+        // modalMap.classList.remove("hidden");
+        // buttonCloseChose.classList.add("hidden");
+        chose[i].classList.add("add-plan__item--select-active");
+      }
+      else {
+        // buttonCloseChose.classList.remove("hidden");
+        chose[i].classList.remove("add-plan__item--select-active");
+      }
+    });
+
+  }
+}
--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,85 @@
+// Вычесление длинны "прогресс бара", смещения stoke dash
 console.log("start script");
 const svgEl = document.querySelectorAll(".level__progress-bar"); // ищем все элементы с уровнем
 svgEl.forEach((svg) => { // цикл
-  const circle = document.getElementById("circle");
-  const dashArray = circle.getTotalLength();
-  const percentFill = svg.dataset.percent;
-  const percent = (dashArray / 100) * (100 - percentFill);
-  svg.style.strokeDasharray = dashArray;
-  svg.style.strokeDashoffset = percent;
+  const circle = document.getElementById("circle"); // svg c тегом "circle"
+  const dashArray = circle.getTotalLength(); // возвращает длинну circle
+  const percentFill = svg.dataset.percent; // берем процент из html разметки, атрибут data-percent="97"
+  const percent = (dashArray / 100) * (100 - percentFill); // вычисление смещения
+  svg.style.strokeDasharray = dashArray; // задаем длину  stroke svg
+  svg.style.strokeDashoffset = percent; // смещение
   console.log(percent);
 });
+// ++++  Открытие меню ++++
+
+const toggleButton = document.querySelector(".toogle");
+const menu = document.querySelector(".menu");
+
+if (toggleButton) {
+  console.log("toggle button find \n");
+
+  toggleButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    console.log("Click toggleButton");
+
+    const logoLight = menu.querySelector(".logo--light");
+    const logoDark = menu.querySelector(".logo--dark");
+
+    if (!toggleButton.classList.contains("toggle--close")) {
+      toggleButton.classList.remove("toggle--open");
+      toggleButton.classList.add("toggle--close");
+      menu.classList.remove("menu--open");
+      menu.classList.add("menu--close");
+
+      if (logoLight && logoDark) {
+        logoLight.classList.add("hidden");
+        logoDark.classList.remove("hidden");
+      }
+      console.log("Close menu");
+    }
+    else { // Открываем меню
+      toggleButton.classList.remove("toggle--close");
+      toggleButton.classList.add("toggle--open");
+      menu.classList.remove("menu--close");
+      menu.classList.add("menu--open");
+
+      if (logoLight && logoDark) {
+        logoDark.classList.add("hidden");
+        logoLight.classList.remove("hidden");
+      }
+      console.log("Open menu");
+    }
+  });
+}
+// +++++++++ Модальное окно+++++++++
+const modalBusiness = document.querySelector(".modal--business-tariff");
+const showButtonTariff = document.querySelector(".show-business-tariff");
+if (modalBusiness) {
+  const closeButtonTariff = modalBusiness.querySelector(".close-tariff");
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (!modalBusiness.classList.contains("hidden")) {
+        evt.preventDefault();
+        modalBusiness.classList.add("hidden");
+        modalBusiness.classList.remove("modal-show");
+      }
+    }
+  });
+
+  showButtonTariff.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    console.log("Click showButtonTariff");
+    modalBusiness.classList.remove("hidden");
+    modalBusiness.classList.add("modal-show");
+    console.log("Show Tariff");
+  });
+
+  closeButtonTariff.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    console.log("Click closeButtonTariff");
+    modalBusiness.classList.add("hidden");
+    modalBusiness.classList.remove("modal-show");
+    console.log("Add class hidden");
+  });
+}
